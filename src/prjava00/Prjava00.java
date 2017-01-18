84339c7fa5f4d132a6cb85d94729c1436e9c6669
guillem <guillem@gmartinez>
2017-01-18 20:23:25 +0100

    Versió 0.2 del projecte prjava00

--- src/prjava00/Prjava00.java
+++ src/prjava00/Prjava00.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package prjava00;
-
-/**
- *
- * @author guillem
- */
+import java.net.*;
 public class Prjava00 {
 
     /**
@@ -18,6 +9,20 @@ public class Prjava00 {
         
         System.out.println("hola mon");
         System.out.println("versio 0.1 del projecte prjava00");
+    try{
+        InetAddress addr = InetAddress.getLocalHost();
+        String ipAddr = addr.getHostAddress();
+        String hostname = addr.getHostName();
+        System.out.println("hostname ="+hostname);
+        System.out.println("Adreça IP: " +ipAddr);
+        System.out.println("Nom de l'usuari: " + System.getProperty("user.name"));
+        System.out.println("Carpeta personal: " + System.getProperty("user.home"));
+        System.out.println("Sistema operatiu: "+ System.getProperty("os.name"));
+        System.out.println("Versió OS: "+ System.getProperty("os.version"));
+        
+    }catch(UnknownHostException e){
+        e.printStackTrace();
+    }
     }
     
 }
