3c12a776ab29a8dc13dc7ef2cff2a925c3a7a706
guillem <guillem@gmartinez>
2017-01-18 19:56:28 +0100

    Versió 0.1 del projecte prjava00

---
+++ src/prjava00/Prjava00.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prjava00;
+
+/**
+ *
+ * @author guillem
+ */
+public class Prjava00 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("hola mon");
+        System.out.println("versio 0.1 del projecte prjava00");
+    }
+    
+}
